fd7a57f0157818b73a3266d4de0e6194f59c547b
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-21 14:21:53 +0700

    delete ProductModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/e-commerce-db'),
-    ProductModule
+    MongooseModule.forRoot('mongodb://localhost:27017/e-commerce-db')
   ],
   controllers: [AppController],
   providers: [AppService],
