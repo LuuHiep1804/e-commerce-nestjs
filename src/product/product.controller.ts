bc7da352fd98f0727b07ffaa9f8e06bcabfdbbfe
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-21 15:58:16 +0700

    create method, update method, delete method, find product method

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateProductDTO } from './product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-    
+    constructor(private productService: ProductService) {}
+
+    @Get('/')
+    async getProducts() {
+        const products = await this.productService.findAllProducts();
+        return products;
+    }
+
+    @Get('/:id')
+    async getProduct(@Param('id') id: string) {
+        const product = await this.productService.findById(id);
+        return product;
+    }
+
+    @Post('/')
+    async create(@Body() createProductDTO: CreateProductDTO) {
+        const product = await this.productService.create(createProductDTO);
+        return product;
+    }
+
+    @Put('/:id')
+    async update(@Param('id') id: string, @Body() createProductDTO: CreateProductDTO) {
+        const product = await this.productService.update(id, createProductDTO);
+        return product;
+    }
+
+    @Delete('/:id')
+    async delete(@Param('id') id: string) {
+        const product = await this.productService.delete(id);
+        return product;
+    }
+
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from 'src/models/product.schema';
+import { CreateProductDTO } from './product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(@InjectModel('Product') private readonly productModel: Model<ProductDocument>) {}
+
+    async findAllProducts(): Promise<Product[]> {
+        return await this.productModel.find().exec();
+    }
+
+    async findById(id: string): Promise<Product> {
+        const product = await this.productModel.findById(id);
+        if(!product) {
+            throw new HttpException('Product not found', HttpStatus.BAD_REQUEST);
+        }
+        return product;
+    }
+
+    async create(productDTO: CreateProductDTO): Promise<Product> {
+        const product = await this.productModel.create({
+            ...productDTO
+        });
+        await product.save();
+        return product;
+    }
+
+    async update(id: string, productDTO: CreateProductDTO): Promise<Product> {
+        const updateProduct = await this.productModel.findByIdAndUpdate(id, productDTO, {new: true});
+        if(!updateProduct) {
+            throw new HttpException('Product not found', HttpStatus.BAD_REQUEST);
+        }
+        return updateProduct;
+    }
+
+    async delete(id: string): Promise<any> {
+        const deleteProduct = await this.productModel.findByIdAndRemove(id);
+        if(!deleteProduct) {
+            throw new HttpException('Product not found', HttpStatus.BAD_REQUEST);
+        }
+        return deleteProduct;
+    }
+
+}
