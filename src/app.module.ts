cffb14bd7ecba7e154faf324ac559fa770023f8d
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-24 22:57:44 +0700

    import category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/e-commerce-db'),
-    ProductModule
+    ProductModule,
+    CategoryModule
   ],
   controllers: [AppController],
   providers: [AppService],
