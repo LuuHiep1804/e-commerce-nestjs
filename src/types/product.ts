56967154ac33d4d4287f9a4598a1984d05eecc00
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-24 22:03:55 +0700

    add type product

---
+++ src/types/product.ts
@@ -0,0 +1,10 @@
+import { Document } from "mongoose";
+
+export interface Product extends Document {
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    title: string;
+    category: string;
+}
