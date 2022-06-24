cef2bc29a0f9fd0cb7bda7fb3764badb465990cc
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-24 23:00:45 +0700

    add category model, type, dto

---
+++ src/models/category.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}})
+export class Category {
+    @Prop()
+    name: string;
+
+    @Prop()
+    description: string;
+
+    @Prop()
+    status: string;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
