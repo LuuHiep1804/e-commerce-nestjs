f5a58bd8c39e74ebbf668bafbe6ffcaeea1c0df3
LuuHiep1804 <luuhiep1804@gmail.com>
2022-06-29 10:59:20 +0700

    feature authorization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,8 +3,11 @@ import { AuthGuard } from '@nestjs/passport';
 import { CreateUserDTO } from 'src/user/user.dto';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
+import { Roles } from './decorators/roles.decorator';
+import { Role } from './enums/role.enum';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
 import { LocalAuthGuard } from './guard/local-auth.guard';
+import { RolesGuard } from './guard/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,13 +19,15 @@ export class AuthController {
         return await this.authService.login(req.user);
     }
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.User)
     @Get('/user')
     async getProfile(@Request() req) {
         return req.user;
     }
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
     @Get('/admin')
     async getDashboard(@Request() req) {
         return req.user;
