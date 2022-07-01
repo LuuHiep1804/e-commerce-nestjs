402230e1e699893992072b0b743ab1162459edc6
LuuHiep1804 <luuhiep1804@gmail.com>
2022-07-01 09:43:49 +0700

    import order module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CategoryModule } from './category/category.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
 import 'dotenv/config';
 
 @Module({
@@ -16,7 +17,8 @@ import 'dotenv/config';
     CategoryModule,
     UserModule,
     AuthModule,
-    CartModule
+    CartModule,
+    OrderModule
   ],
   controllers: [AppController],
   providers: [AppService],
