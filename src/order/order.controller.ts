f2ade66524551622d080133a1f07f352d19cbdca
LuuHiep1804 <luuhiep1804@gmail.com>
2022-07-06 21:43:27 +0700

    route create and update order

---
+++ src/order/order.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { Request } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enums/role.enum';
+import { OrderDTO } from './order.dto';
+
+@Controller('order')
+export class OrderController {
+    constructor(private orderService: OrderService) {}
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.User)
+    @Post('/')
+    async create(@Request() req) {
+        const userId = req.user.userId;
+        const order = await this.orderService.createOrder(userId);
+        return order;
+    }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
+    @Put('/:id')
+    async update(@Param('id') orderId: string, @Body() orderDTO: OrderDTO) {
+        const order = await this.orderService.updateOrder(orderId, orderDTO);
+        return order;
+    }
+
+}
