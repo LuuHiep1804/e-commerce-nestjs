0a1e4da8dcb99702d774273a2d259dc9bf45da33
LuuHiep1804 <luuhiep1804@gmail.com>
2022-07-07 11:50:42 +0700

    add authorization

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enums/role.enum';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { FilterProductDTO } from './filter-product.dto';
 import { CreateProductDTO } from './product.dto';
 import { ProductService } from './product.service';
@@ -24,18 +28,24 @@ export class ProductController {
         return product;
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
     @Post('/')
     async create(@Body() createProductDTO: CreateProductDTO) {
         const product = await this.productService.create(createProductDTO);
         return product;
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
     @Put('/:id')
     async update(@Param('id') id: string, @Body() createProductDTO: CreateProductDTO) {
         const product = await this.productService.update(id, createProductDTO);
         return product;
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.Admin)
     @Delete('/:id')
     async delete(@Param('id') id: string) {
         const product = await this.productService.delete(id);
